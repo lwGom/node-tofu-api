3fb3882d57f6cf61b7ac43e7febafd905aef427b
lwGom <imdancinginthebrain@gmail.com>
2019-04-04 04:15:56 +0100

    Add tests for GET/tofus

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,16 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Tofu} = require('./../models/tofu')
 
+const tofus = [{
+    text: 'First test tofu'
+}, {
+    text: 'Second test tofu'
+}]
+
 beforeEach((done) => {
     Tofu.remove({}).then(() => {
-        done()
-    })
+        return Tofu.insertMany(tofus)
+    }).then(() => done())
 })
 
 describe('Post /tofus', () => {
@@ -28,7 +34,7 @@ describe('Post /tofus', () => {
                     return done(err)
                 }
 
-            Tofu.find().then((tofus) => {
+            Tofu.find({text}).then((tofus) => {
                 expect(tofus.length).toBe(1)
                 expect(tofus[0].text).toBe(text)
                 done()
@@ -48,10 +54,23 @@ describe('Post /tofus', () => {
                     return done(err)
                 }
                 Tofu.find().then((tofus) => {
-                    expect(tofus.length).toBe(0)
+                    expect(tofus.length).toBe(2)
                     done()
                 }).catch((e) => done(e))
             })
 
+    })
+})
+
+
+describe('GET /tofus', () => {
+    it('should get all tofus', (done) => {
+        request(app)
+            .get('/tofus')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.tofus.length).toBe(2)
+            })
+            .end(done)
     })
 })
