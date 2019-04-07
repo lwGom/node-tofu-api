c686fa33301f49c94dbf3b0638c4ed72181d68bf
lwGom <imdancinginthebrain@gmail.com>
2019-04-08 00:40:53 +0100

    Add test cases fir GET /tofus/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,27 @@
 const expect = require('expect')
 const request = require('supertest')
+const {ObjectId} = require('mongodb')
 
 const {app} = require('./../server')
 const {Tofu} = require('./../models/tofu')
 
 const tofus = [{
+    _id: new ObjectId(),
     text: 'First test tofu'
 }, {
+    _id: new ObjectId(),
     text: 'Second test tofu'
 }]
+
+
 
 beforeEach((done) => {
     Tofu.remove({}).then(() => {
         return Tofu.insertMany(tofus)
     }).then(() => done())
 })
+
+
 
 describe('Post /tofus', () => {
     it('should create a new tofu', (done) => {
@@ -74,3 +81,34 @@ describe('GET /tofus', () => {
             .end(done)
     })
 })
+
+
+describe('GET /tofus/:id', () => {
+    it('Should return tofu doc', (done) => {
+        request(app)
+            .get(`/tofus/${tofus[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.tofu.text).toBe(tofus[0].text)
+            })
+            .end(done)
+    })
+
+    it('Should return 404 if todo not found', (done) => {
+        var hexId = new ObjectId().toHexString()
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done)            
+    })
+
+    it('Should return 404 for non-object ids', (done) => {
+        
+        request(app)
+            .get(`/todos/123abc`)
+            .expect(404)
+            .end(done)            
+    })
+})
+
+
